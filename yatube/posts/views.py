7a269f4fc745e2f44554f81d7e38430492610c72
Denis Kudakov <denn88kudakov@yandex.ru>
2022-01-31 14:26:48 +0300

    Исправил замечания

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,51 +1,36 @@
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import PostForm
 from .models import Group, Post, User
+from .paginator import get_paginator
 
 
 def index(request):
-    template = 'posts/index.html'
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list, settings.SHOW_POSTS)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        'page_obj': page_obj,
-    }
-    return render(request, template, context)
+    context = get_paginator(Post.objects.all(), request)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    template = 'posts/group_list.html'
     post_list = group.posts.all()
-    paginator = Paginator(post_list, settings.SHOW_POSTS)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'page_obj': page_obj,
     }
-    return render(request, template, context)
+    context.update(get_paginator(post_list, request))
+    return render(request, 'posts/group_list.html', context)
 
 
 def profile(request, username):
     # Здесь код запроса к модели и создание словаря контекста
     author = get_object_or_404(User, username=username)
     post_list = Post.objects.filter(author=author)
-    paginator = Paginator(post_list, settings.SHOW_POSTS)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
     post_count = author.posts.count()
     context = {
         'author': author,
-        'page_obj': page_obj,
         'post_count': post_count,
     }
+    context.update(get_paginator(post_list, request))
     return render(request, 'posts/profile.html', context)
 
 
